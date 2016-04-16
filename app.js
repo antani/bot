d42fdd05b6ab7d15731a4675c7a3f41f08c411df
Ved Antani <ved.antani@myntra.com>
2016-04-16 11:12:46 +0530

    generic template

--- app.js
+++ app.js
@@ -26,35 +26,100 @@ app.get('/webhook', function (req, res) {
     res.send('Error, wrong validation token');    
   }
 });
+function sendTextMessage(sender, text) {
+    messageData = {
+        text:text
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
 
-let bot = new Bot({
-  verify: 'myntra_bot',
-  token: 'CAALiIkjYEW0BAOW8uyvBDDbc7urRruwNXEMC7dypYP1VRjUM9nTw2dmsvNBn8XZA1lH6xNIDNXmKfIW6EZBQdnxqOZAKV9atQ9gsy0jw6eyVaCCZBsTeImZBHjtk4lfar3S8VZCDpieYx7ZAsLnZAKGuf2ZBIX7X0AfyiMLxulpUHtR5eO8F7FCk02jGs7pzYY9gZD',
+function sendGenericMessage(sender) {
+    messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "First card",
+                    "subtitle": "Element #1 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": "https://www.messenger.com",
+                        "title": "web url"
+                    }, {
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for first element in a generic bubble",
+                    }],
+                }, {
+                    "title": "Second card",
+                    "subtitle": "Element #2 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+                    "buttons": [{
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for second element in a generic bubble",
+                    }],
+                }]
+            }
+        }
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
+app.post('/webhook/', function (req, res) {
+    messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
+        if (event.message && event.message.text) {
+            text = event.message.text
+            if (text === 'Generic') {
+                sendGenericMessage(sender)
+                continue
+            }
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+        if (event.postback) {
+            text = JSON.stringify(event.postback)
+            sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
+            continue
+        }
+    }
+    res.sendStatus(200)
 })
 
-bot.on('message', (payload, reply) => {
-  console.log("Somewhere in bot.on");
-  console.log("--"+ payload)
-  reply({ text: 'Will show you some good stuff...hold on.' })
-  // getSearchResults((err, payload) => {
-  //   if (err) throw err
-  //   let element = {
-  //     title: payload.title,
-  //     subtitle: payload.price,
-  //     image_url: payload.image || null,
-  //     buttons: []
-  //   }
-  //   reply({
-  //     attachment: {
-  //       type: 'template',
-  //       payload: {
-  //         template_type: 'generic',
-  //         elements: [element]
-  //       }
-  //     }
-  //   })
-  // });
-})
+
+
 lex.listen([80], [443, 5001], function () {
   var protocol = ('requestCert' in this) ? 'https': 'http';
   console.log("Listening at " + protocol + '://localhost:' + this.address().port);
