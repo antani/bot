d81701ec023446a2f5f4428d50e212fcd9561baa
root <root@ved.im>
2016-04-15 22:06:52 -0400

    reverted to plain old express but will use localtunnel for now

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ var fs = require('fs'),
     express = require('express'),
     app = express();
 
-    https.createServer({
-      key: fs.readFileSync('newkey.pem'),
-      cert: fs.readFileSync('cert.pem')
-    }, app).listen(443);
 
     app.get('/', function (req, res) {
       res.header('Content-type', 'text/html');
       return res.end('<h1>Hello, Secure World!</h1>');
     });
+    app.listen(3000,function() {
+	console.log('Started at 3000');
+    });
+    app.get('/webhook', function (req, res) {
+      if (req.query['hub.verify_token'] === 'myntra_bot') {
+    	res.send(req.query['hub.challenge']);
+      } else {
+    	res.send('Error, wrong validation token');    
+      }
+});
